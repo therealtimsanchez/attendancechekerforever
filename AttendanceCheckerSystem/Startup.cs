85d26c25ed0b68ca06154333df7b68922980a282
therealtimsanchez55 <therealtimsanchez55@gmail.com>
2017-12-12 13:45:53 -0600

    Commit

--- AttendanceCheckerSystem/Startup.cs
+++ AttendanceCheckerSystem/Startup.cs
@@ -26,8 +26,10 @@ namespace AttendanceCheckerSystem
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("AzureConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
